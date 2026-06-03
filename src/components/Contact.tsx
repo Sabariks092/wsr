08ca68bd47575e3fd4ac7a750295d72502db449b
Sabari Shanmuga Priyan K <sabariks092@gmail.com>
2026-06-04 01:49:18 +0530

    90% draft completed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -223,6 +223,8 @@ export default function Contact() {
                     <option className="bg-[#0D0D0D] text-vintage-beige" value="promotion-collaboration">Promotion Collaboration</option>
                     <option className="bg-[#0D0D0D] text-vintage-beige" value="donation">Donation</option>
                     <option className="bg-[#0D0D0D] text-vintage-beige" value="threatening">Threatening</option>
+                    <option className="bg-[#0D0D0D] text-vintage-beige" value="threatening">APPRECIATION</option>
+                    <option className="bg-[#0D0D0D] text-vintage-beige" value="threatening">SOMMA</option>
                   </select>
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-[#D9B06A]">
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
