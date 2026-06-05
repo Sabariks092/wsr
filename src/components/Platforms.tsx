928223ae70ee4b9257348894d9a17b2437027ef8
Sabari Shanmuga Priyan K <sabariks092@gmail.com>
2026-06-06 04:22:47 +0530

    90 percent updated

--- src/components/Platforms.tsx
+++ src/components/Platforms.tsx
@@ -10,9 +10,9 @@ const Platforms = () => {
     <div className='flex justify-center gap-3 items-center lg:my-4'>
         <a href="https://podcasts.apple.com/in/podcast/wild-south-raiders-podcast/id1833613189"><Image src={apple} alt="Apple Podcasts"   className=' w-8 h-8 md:w-12 md:h-12 shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
         <a href="https://music.amazon.co.uk/podcasts/44983700-5ce0-4f6e-94ba-4abe9abe7765/wild-south-raiders-podcast"><Image src={amazon} alt="Amazon Music"  className=' w-8 h-8 md:w-12 md:h-12 shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
-        <a href="https://open.spotify.com/show/3Vyzr177KDIGxUPjEHzfMD"><Image src={spotify} alt="Spotify"  className=' w-8 h-8 md:w-12 md:h-12shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
-        <a href="https://castbox.fm/channel/Wild-South-Raiders-Podcast-id6713477?country=gb"><Image src={cloudsound} alt="Google Podcasts"  className=' w-8 h-8 md:w-12 md:h-12shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
-        <a href="https://pocketcasts.com/podcast/wild-south-raiders-podcast/045459f0-5a2d-013e-fb5b-0affd256cbb1"><Image src={pocketcasts} alt="Pocket Casts" className=' w-8 h-8 md:w-12 md:h-12shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
+        <a href="https://open.spotify.com/show/3Vyzr177KDIGxUPjEHzfMD"><Image src={spotify} alt="Spotify"  className=' w-8 h-8 md:w-12 md:h-12 shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
+        <a href="https://castbox.fm/channel/Wild-South-Raiders-Podcast-id6713477?country=gb"><Image src={cloudsound} alt="Google Podcasts"  className=' w-8 h-8 md:w-12 md:h-12 shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
+        <a href="https://pocketcasts.com/podcast/wild-south-raiders-podcast/045459f0-5a2d-013e-fb5b-0affd256cbb1"><Image src={pocketcasts} alt="Pocket Casts" className=' w-8 h-8 md:w-12 md:h-12 shadow-[0_2px_8px_rgba(0,0,0,0.5)] hover:scale-110 transition-all duration-200'/></a>
     </div>
   )
 }
